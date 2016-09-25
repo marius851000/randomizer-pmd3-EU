9c5817444d91202f441e5c453ee61c70581918e8
marius851000 <mariusdavid@laposte.net>
2016-09-25 15:39:13 +0200

    mise à jour du randomizer, et ajout du readme.md

---
+++ EU ran.bat
@@ -0,0 +1,16 @@
+mkdir rom
+ndstool.exe -x "rom-EU.nds" -9 "rom/arm9.bin" -7 "rom/arm7.bin" -y9 "rom/y9.bin" -y7 "rom/y7.bin" -d "rom/data" -y "rom/overlay" -t "rom/banner.bin" -h "rom/header.bin" -v
+
+ppmd_kaoutil.exe "./rom/data/FONT/kaomado.kao" "./portraits"
+ppmd_packfileutil.exe "rom/data/MONSTER/m_ground.bin"
+
+echo "fichier extrait"
+
+randomizer.py
+
+call ppmd_kaoutil.exe "portraits" "rom/data/FONT/kaomado.kao"
+
+ppmd_packfileutil.exe "rom/data/MONSTER/m_ground"
+
+del "rom.nds"
+ndstool.exe -c "pmd-random.nds" -9 "rom/arm9.bin" -7 "rom/arm7.bin" -y9 "rom/y9.bin" -y7 "rom/y7.bin" -d "rom/data" -y "rom/overlay" -t "rom/banner.bin" -h "rom/header.bin"
--- randomizer.py
+++ randomizer.py
@@ -106,25 +106,20 @@ def pokesprite():
         shutil.copy(rep+"/m_go/"+a[loop],rep+"/rom/data/MONSTER/m_ground/"+a[loop])
 
 def overworld():
+    total="/7 : "
     rep=os.getcwd()
     file=open('equiv.txt')
     temp="None"
     table=[]
     while temp != "":
+        print("1"+total+"?")
         temp=file.readline()
         if temp!="":
             temp2=temp.split("\n")
             temp2=temp2[0]
-            print(temp2)
             table.append(temp2.split("-"))
-    print('lecture de equiv.txt achever')
     file.close()
-    for loop in range(len(table)):
-        print('---')
-        print(loop)
-        print(table[loop][0])
-        print(table[loop][1])
-
+    
     """"code basé sur la fonction portrait"""
     rep=os.getcwd()
     os.chdir(rep)
@@ -139,50 +134,46 @@ def overworld():
         os.mkdir('m_go')
     except:
         pass
-    for loop in range(len(a)):
-        print('---')
+    portemp=0
+    
+    for loop in range(len(table)):
+        print("2"+total+str(loop)+"/"+str(len(table)))
         randomn=randrange(0,len(table))
-        choixsp="m_ground_0000_0x1300"
-
-        if loop<len(table):
-            for loop2 in range(len(table)):
-                print(table[loop2][1]+" vs "+table[randomn][1])
-                if table[loop2][1]==table[randomn][1]:
-                    choixsp=table[randomn][1]
-                    print('win')
-            por="0001"
-            for loop2 in range(len(table)):
-                print(table[loop2][0]+" vs "+table[randomn][0])
-                if table[loop2][0]==table[randomn][0]:
-                    por=table[loop2][0]
-                    print('win')
+        spor=table[randomn][1]
+        poror=table[randomn][0]
+        porto=table[loop][0]
+        spto=table[loop][1]
+        select=1
         
-        print(choixsp)
-        b=os.listdir(rep+"/portraits/"+por)
-        print(b)
-        try:
-            os.mkdir(rep+'/porrandom/'+a[loop])
-        except:
-            pass
 
-        for loop2 in range(len(b)):
-            print('copy')
-            shutil.copy(rep+"/portraits/"+por+"/"+b[loop2],rep+"/porrandom/"+a[loop]+"/"+b[loop2])
-        if loop<len(c):
-            shutil.copy(rep+"/rom/data/MONSTER/m_ground/"+choixsp+".wan",rep+"/m_go/"+c[loop])
-        print(table[randomn][1])
-    
-    
+        if select!=0:
+            b=os.listdir(rep+"/portraits/"+poror)
+            try:
+                os.mkdir(rep+'/porrandom/'+porto)
+            except:
+                pass
+            for loop2 in range(len(b)):
+                shutil.copy(rep+"/portraits/"+poror+"/"+b[loop2],rep+"/porrandom/"+porto+"/"+b[loop2])
+            shutil.copy(rep+"/rom/data/MONSTER/m_ground/"+spor+".wan",rep+"/m_go/"+spto+".wan")
+
+    for loop in range(len(c)):
+        print("3"+total+str(loop)+"/"+str(len(c)))
+        if os.path.isfile(rep+"/m_go/"+c[loop])==False:
+            randomn=randrange(0,len(c))
+            shutil.copy(rep+"/rom/data/MONSTER/m_ground/"+c[randomn],rep+"/m_go/"+c[loop])
     
     for loop in range(len(a)):
+        print("4"+total+str(loop)+"/"+str(len(a)))
         b=os.listdir(rep+"/portraits/"+a[loop])
         for loop2 in range(len(b)):
             os.remove(rep+"/portraits/"+a[loop]+'/'+b[loop2])
         os.rmdir(rep+"/portraits/"+a[loop])
     os.rmdir(rep+"/portraits")
-    
+
+    a=os.listdir(rep+'/porrandom')
     os.mkdir(rep+"/portraits")
     for loop in range(len(a)):
+        print("5"+total+str(loop)+"/"+str(len(a)))
         b=os.listdir(rep+"/porrandom/"+a[loop])
         os.mkdir(rep+"/portraits/"+a[loop])
         for loop2 in range(len(b)):
@@ -198,10 +189,13 @@ def overworld():
                     shutil.copy(rep+"/portraits/"+a[loop]+"/0000.png",rep+"/portraits/"+a[loop]+"/00"+str(loop2)+".png")
                 except:
                     pass
-                
+
+    c=os.listdir(rep+'/m_go')
     for loop in range(len(c)):
+        print("6"+total+str(loop)+"/"+str(len(c)))
         os.remove(rep+"/rom/data/MONSTER/m_ground/"+c[loop])
     for loop in range(len(c)):
+        print("7"+total+str(loop)+"/"+str(len(c)))
         shutil.copy(rep+"/m_go/"+c[loop],rep+"/rom/data/MONSTER/m_ground/"+c[loop])
 
 
